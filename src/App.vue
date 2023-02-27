02b522d9d0b78002e3a5e54f0f3e47a3a05a5ad8
JSKUO <1455749306@qq.com>
2023-02-27 13:08:16 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+
+  <div>
+    <div>父组件</div>
+    <HelloWorld title="测试hooks" a="lilili" ref="Hello" @on-click="getName" :value="a"></HelloWorld>
+    <button @click="accept">接收</button>
+  </div>
+
+</template>
+
+<script setup lang='ts'>
+import { ref,reactive } from 'vue'
+import HelloWorld from './components/HelloWorld.vue';
+
+const Hello = ref<InstanceType<typeof HelloWorld>>()
+
+const a = ref('父组件传过来的')
+const getName =(value:String) =>{
+  console.log(value);
+  
+}
+
+
+const accept = () => {
+  console.log(Hello.value?.name,Hello.value?.open);
+}
+</script>
+<style scoped lang='less'>
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,48 @@
+<template>
+
+  <div class="content">
+    <div>子组件</div>
+    <div>我是子组件</div>
+    <div>{{ value }}</div>
+    <button type="button" @click="goFather">传值</button>
+  </div>
+  
+
+</template>
+
+<script setup lang='ts'>
+import { ref,reactive, useAttrs } from 'vue'
+const props = defineProps({
+  value:{
+    type:String
+  }
+})
+
+const attrs = useAttrs()
+console.log(attrs);
+
+
+
+console.log(props.value);
+const name = ref('xiaoli')
+const emit = defineEmits(['on-click'])
+
+const goFather = () =>{
+  emit('on-click','我是子组件传过去的值')
+}
+
+
+defineExpose({
+  name:name,
+  open: ()=>{
+    console.log('利用暴露Expose传值');
+  }
+})
+</script>
+<style scoped lang='less'>
+.content{
+  width: 400px;
+  height: 200px;
+  border: 1px solid red;
+}
+</style>
